7e53f87259cbd28bbae07aa2c82aa10759ac7585
Rian Martins <rianlopes@hotmail.com>
2018-06-11 14:54:55 +0000

    Nova tabela produtos

---
+++ database/migrations/2018_06_11_130116_produtos.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Produtos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome_produto');
+	    $table->string('setor_produto');
+	    $table->string('descrition');
+            $table->timestamp('created_at')->nullable();
+	    $table->timestamp('update_at')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
