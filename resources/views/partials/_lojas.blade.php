4fcb0c8f5cf4758986052e83ca9842f0a8c27e81
Rian Martins <rianlopes@hotmail.com>
2018-06-12 05:52:11 +0000

    teste

--- resources/views/partials/_lojas.blade.php
+++ resources/views/partials/_lojas.blade.php
@@ -5,33 +5,33 @@
       <div class="brands-agile">
         <div class="col-md-2 w3layouts-brand">
           <div class="brands-w3l">
-            <p><a href="www.extra.com"><img style="height: 100%; width: 100%" src="images/extra_logo.png">Extra</img></a></p>
+            <p><a href="www.extra.com.br"><img style="height: 100%; width: 100%" src="images/extra_logo.png">Extra</img></a></p>
           </div>
         </div>
         <div class="col-md-2 w3layouts-brand">
           <div class="brands-w3l">
-            <p><a href="#">Lorem</a></p>
+          <p><a href="www.magazineluiza.com.br"><img style="height: 100%; width: 100%" src="images/magalu_logo.png">Extra</img></a></p>
           </div>
         </div>
         <div class="col-md-2 w3layouts-brand">
           <div class="brands-w3l">
-            <p><a href="#">Lorem</a></p>
+          <p><a href="www.casasbahia.com.br"><img style="height: 100%; width: 100%" src="images/casasbahia_logo.png">Extra</img></a></p>
           </div>
         </div>
 
         <div class="col-md-2 w3layouts-brand">
           <div class="brands-w3l">
-            <p><a href="#">Lorem</a></p>
+          <p><a href="www.carrefour.com.br"><img style="height: 100%; width: 100%" src="images/carrefour_logo.png">Extra</img></a></p>
           </div>
         </div>
         <div class="col-md-2 w3layouts-brand">
           <div class="brands-w3l">
-            <p><a href="#">Lorem</a></p>
+          <p><a href="www.americanas.com.br"><img style="height: 100%; width: 100%" src="images/americanas_logo.png">Extra</img></a></p>
           </div>
         </div>
         <div class="col-md-2 w3layouts-brand">
           <div class="brands-w3l">
-            <p><a href="#">Lorem</a></p>
+          <p><a href="www.saraiva.com"><img style="height: 100%; width: 100%" src="images/saraiva_logo.jpg">Extra</img></a></p>
           </div>
         </div>
         <div class="clearfix"></div>
@@ -39,23 +39,23 @@
       <div class="brands-agile-1">
         <div class="col-md-2 w3layouts-brand">
           <div class="brands-w3l">
-            <p><a href="#">Lorem</a></p>
+          <p><a href="www.shoptime.com"><img style="height: 100%; width: 100%" src="images/shoptime_logo.png">Extra</img></a></p>
           </div>
         </div>
         <div class="col-md-2 w3layouts-brand">
           <div class="brands-w3l">
-            <p><a href="#">Lorem</a></p>
+          <p><a href="www.pontofrio.com"><img style="height: 100%; width: 100%" src="images/pontofrio_logo.png">Extra</img></a></p>
           </div>
         </div>
         <div class="col-md-2 w3layouts-brand">
           <div class="brands-w3l">
-            <p><a href="#">Lorem</a></p>
+          <p><a href="www.submarino.com"><img style="height: 100%; width: 100%" src="images/submarino_logo.png">Extra</img></a></p>
           </div>
         </div>
 
         <div class="col-md-2 w3layouts-brand">
           <div class="brands-w3l">
-            <p><a href="#">Lorem</a></p>
+          <p><a href="www.walmart.com"><img style="height: 100%; width: 100%" src="images/walmart_logo.jpg">Extra</img></a></p>
           </div>
         </div>
         <div class="col-md-2 w3layouts-brand">
